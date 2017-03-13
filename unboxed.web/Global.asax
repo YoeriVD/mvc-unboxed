e3b65da5703a59ac858dbbf13e0ebf8aa28fcb72
yoeri <yoeri@DESKTOP-S3DR6O1>
2017-03-13 20:37:37 +0100

    Add project files.

---
+++ unboxed.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="unboxed.web.MvcApplication" Language="C#" %>
---
+++ unboxed.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(unboxed.web.Startup))]
+namespace unboxed.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
